b060670c9762d89ed9e1ea41a3d9434b114cb67a
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-02 10:04:08 +0530

    Add files via upload

---
+++ symmetric.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int **a;
+    int i, j, n;
+
+    cout << "Enter dimension:" << endl;
+    cin >> n;
+
+    // Allocate memory for the 2D array
+    a = new int *[n];
+    for (i = 0; i < n; i++) {
+        a[i] = new int[n];
+    }
+
+    cout << "Enter elements:" << endl;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Check if the matrix is symmetric
+    bool symmetric = true;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+        if (!symmetric) {
+            break;
+        }
+    }
+
+    // Print the matrix
+    if (symmetric) {
+        cout << "The entered matrix is symmetric:" << endl;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "The entered matrix is not symmetric." << endl;
+    }
+
+    // Deallocate memory
+    for (i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+
+    return 0;
+}
