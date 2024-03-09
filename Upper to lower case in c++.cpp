8ff93192fcf7d1cee9877cba80f282110545266f
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-09 21:41:38 +0530

    Upper to lower case in c++.cpp

--- Upper to lower case in c++.cpp
+++ Upper to lower case in c++.cpp
@@ -1,14 +1,16 @@
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     char name[]="mokshada";
-//     for(int i=0;name[i]!='\0';i++)
-//     {
-//         name[i]=name[i]-32;
-//     }
-//         cout<<name;
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[]="mokshada";
+    for(int i=0;name[i]!='\0';i++)
+    {
+        name[i]=name[i]-32;
+    }
+        cout<<name;
+}
+
+// SECOND METHOD
 
 #include<iostream>
 using namespace std;
