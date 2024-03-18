c73aab5134c943d57356afe71eac12d959eb5372
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-18 18:42:46 +0530

    CountVowels.cpp

--- GeeksForGeeks/CountVowels.cpp
+++ GeeksForGeeks/CountVowels.cpp
@@ -17,3 +17,30 @@ int main()
 
     return 0;
 }
+
+// Second method
+#include<iostream> 
+using namespace std; 
+ 
+bool isVowel(char ch) 
+{ 
+	ch = toupper(ch); 
+	return (ch=='A' || ch=='E' || ch=='I' || 
+					ch=='O' || ch=='U'); 
+} 
+ 
+int countVowels(string str) 
+{ 
+	int count = 0; 
+	for (int i=0; i<str.length(); i++) 
+		if (isVowel(str[i])) 
+			++count; 
+	return count; 
+} 
+
+int main() 
+{ 
+	string str = "abc de"; 
+	cout << countVowels(str) << endl; 
+	return 0; 
+} 
