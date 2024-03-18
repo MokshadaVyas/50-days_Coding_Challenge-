91066008e58d681ba935cb6f795a8d66d8f06cb0
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-18 18:39:23 +0530

    MultiplicationTable.cpp

--- GeeksForGeeks/MultiplicationTable.cpp
+++ GeeksForGeeks/MultiplicationTable.cpp
@@ -14,3 +14,16 @@ int main()
 
     return 0;
 }
+
+// second method
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 5; 
+	for (int i = 1; i <= 10; ++i) 
+		cout << n << " * " << i << " = "
+			<< n * i << endl;
+    return 0;
+}
