a87e7154d61146e24f3f0c34f0ee918d77dcc1b0
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-20 20:36:02 +0530

    Sum_of_all_Elements.cpp

--- GeeksForGeeks/SumofallElements.cpp
+++ GeeksForGeeks/SumofallElements.cpp
@@ -21,3 +21,29 @@ int main()
     cout<<"Sum of the given number is: "<<sum(n);
     return 0;
 }
+
+// Second method ---when number is given
+#include <bits/stdc++.h>
+using namespace std;
+class gfg {
+public:
+	int getSum(int n)
+	{
+		int sum = 0;
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+		return sum;
+	}
+};
+
+int main()
+{
+	gfg g;
+	int n = 687;
+
+	// Function call
+	cout << g.getSum(n);
+	return 0;
+}
