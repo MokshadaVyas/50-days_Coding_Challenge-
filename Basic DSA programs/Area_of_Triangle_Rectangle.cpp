cba6a385920906c5a06cd58dc478683c1d4e6326
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-03-21 21:06:25 +0530

    Area_of_Triangle_Rectangle.cpp

--- Basic DSA programs/Area_of_Triangle_Rectangle.cpp
+++ Basic DSA programs/Area_of_Triangle_Rectangle.cpp
@@ -40,3 +40,107 @@ int main()
     cout<<"Area of rectangle is: "<<r1.area()<<endl;
     return 0;
 }
+
+// Second method-----
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual float area() = 0;
+};
+
+class Triangle : public Shape {
+private:
+    float base;
+    float height;
+
+public:
+    Triangle(float b, float h) : base(b), height(h) {}
+
+    float area() override {
+        return 0.5 * base * height;
+    }
+};
+
+class Rectangle : public Shape {
+private:
+    float length;
+    float width;
+
+public:
+    Rectangle(float l, float w) : length(l), width(w) {}
+
+    float area() override {
+        return length * width;
+    }
+};
+
+int main() {
+    float base, height, length, width;
+
+    cout << "Enter base and height of the triangle: ";
+    cin >> base >> height;
+
+    Triangle triangle(base, height);
+    cout << "Area of triangle: " << triangle.area() << endl;
+
+    cout << "Enter length and width of the rectangle: ";
+    cin >> length >> width;
+
+    Rectangle rectangle(length, width);
+    cout << "Area of rectangle: " << rectangle.area() << endl;
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual float area() = 0;
+};
+
+class Triangle : public Shape {
+private:
+    float base;
+    float height;
+
+public:
+    Triangle(float b, float h) : base(b), height(h) {}
+
+    float area() override {
+        return 0.5 * base * height;
+    }
+};
+
+class Rectangle : public Shape {
+private:
+    float length;
+    float width;
+
+public:
+    Rectangle(float l, float w) : length(l), width(w) {}
+
+    float area() override {
+        return length * width;
+    }
+};
+
+int main() {
+    float base, height, length, width;
+
+    cout << "Enter base and height of the triangle: ";
+    cin >> base >> height;
+
+    Triangle triangle(base, height);
+    cout << "Area of triangle: " << triangle.area() << endl;
+
+    cout << "Enter length and width of the rectangle: ";
+    cin >> length >> width;
+
+    Rectangle rectangle(length, width);
+    cout << "Area of rectangle: " << rectangle.area() << endl;
+
+    return 0;
+}
